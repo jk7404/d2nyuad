a7befd0230eb0b02a1ebecdd06c31408a91108ac
rlawogus6514 <93790547+rlawogus6514@users.noreply.github.com>
2021-11-06 12:41:53 +0400

    Update and rename Read.js to read.js

---
+++ functions/read.js
@@ -0,0 +1,14 @@
+import { createClient } from "@supabase/supabase-js"
+
+function readNum() {
+	console.log("Yes");
+	url = 'https://uxdydupspijujofdyxug.supabase.co'
+	key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjM2MTc0MzIzLCJleHAiOjE5NTE3NTAzMjN9.M3_cTtMyV5eDLo8QLlyOgpD2YS4uGDz9DLIKQca7Rvs'
+	const supabase = createClient(url, key)
+
+	let { data: D2NYUAD, error } = await supabase
+		.from('D2NYUAD')
+		.select('*')
+	console.log(data);
+	return data
+}
