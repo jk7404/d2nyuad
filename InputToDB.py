27016d1969cb3837f95b79e93a386c1dad418118
rlawogus6514 <93790547+rlawogus6514@users.noreply.github.com>
2021-11-07 17:04:29 +0400

    Update InputToDB.py

--- InputToDB.py
+++ InputToDB.py
@@ -1,6 +1,7 @@
 from supabase import create_client, Client
-import time; 
+import time
 import requests
+import random
 
 #Access the DB for data input.
 key: str = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjM2MTc0MzIzLCJleHAiOjE5NTE3NTAzMjN9.M3_cTtMyV5eDLo8QLlyOgpD2YS4uGDz9DLIKQca7Rvs'
@@ -13,15 +14,18 @@ header['Content-Type'] = 'application/json'
 header['Prefer'] = 'return=representation'
 
 #Count time and write the number of people in D2 for each timestamp.
-time = int(time.time())
-timecount = 0
-num = 3
+wait = int(time.time())
+start = wait
+num = 30
+n = 3
 
 #Data is overwritten every 5 minutes.
 while True:
-    data = { "time": time, "num": num }
-    response = requests.post(f'https://uxdydupspijujofdyxug.supabase.co/rest/v1/D2NYUAD', headers=header, json=data)
-    time = int(time.time())
-    timecount += 300
-    num += 5
-    print(response.text)
+    if ((wait - start) % 5 == 0):
+        data = { "time": wait, "num": num }
+        response = requests.post(f'https://uxdydupspijujofdyxug.supabase.co/rest/v1/D2NYUAD', headers=header, json=data)
+        num += random.randint(-3, 10)
+        if num >= 80:
+            num -= 8
+        print(response.text)
+    wait = int(time.time())
